15d0165cd049f83686528ef764b798d0617dcac8
sabbir1452 <sabbirrahman1452@gmail.com>
2023-12-17 13:32:06 +0600

    second commit

--- comparison.js
+++ comparison.js
@@ -10,4 +10,19 @@
 
 // greater than or equal
 // console.log(5 >= 6);
-console.log(5 >= 2);
+// console.log(5 >= 2);
+// console.log(2 >= 2);
+
+var num1 = 56;
+var num2 = 89;
+
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+// multiple conditions
+// money1 > money2 && result1 > result2 && height1 > height2;
+money1 > money2 || result1 > result2 || height1 > height2;
