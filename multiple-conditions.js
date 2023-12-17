36e213add4059786c755f0d2817ccc9c20dc0c50
sabbir1452 <sabbirrahman1452@gmail.com>
2023-12-17 14:07:06 +0600

    lsfjd

---
+++ multiple-conditions.js
@@ -0,0 +1,37 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if (isGraduated === true) {
+//   console.log("Eso biye kore feli");
+// } else {
+//   console.log("na ami ekhon biye korte parbo na");
+// }
+
+// if (isGraduated === true && salary > 50000) {
+//   console.log("Eso biye kore feli");
+// } else {
+//   console.log("na ami ekhon biye korte parbo na");
+// }
+
+// if (isGraduated === true && salary > 50000 && cars >= 1) {
+//   console.log("Eso biye kore feli");
+// } else {
+//   console.log("na ami ekhon biye korte parbo na");
+// }
+
+var isGraduated = false;
+var salary = 61000;
+var cars = 1;
+
+// if (isGraduated === true || salary > 50000 || cars >= 1) {
+//   console.log("Eso Prem Kori");
+// } else {
+//   console.log("tomar friend er mobiel number dao");
+// }
+
+if ((isGraduated === true && salary > 50000) || cars >= 1) {
+  console.log("biye to korei sarbo");
+} else {
+  console.log("na ami toke biye korbo na");
+}
