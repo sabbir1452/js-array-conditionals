679f8aaf5e11308f4bc1c7078e17df413444f2f5
sabbir1452 <sabbirrahman1452@gmail.com>
2023-12-17 13:25:56 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,13 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 == 6);
+// console.log(6 == 6);
+// console.log(5 != 6);
+// console.log(15 != 15);
+// less than or equal
+// console.log(5 <= 6);
+// console.log(16 <= 6);
+
+// greater than or equal
+// console.log(5 >= 6);
+console.log(5 >= 2);
---
+++ push-pop.js
@@ -0,0 +1,15 @@
+var numbers = [78, 45, 98, 45];
+// use push to add element to an array as the last element array
+// number.push(63);
+// console.log(number);
+var friends = ["balam", "kalam", "salam"];
+friends.push("gelam");
+friends.push("pailam");
+// console.log(friends);
+
+// use pop to get last element
+console.log(numbers);
+// numbers.pop();
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
