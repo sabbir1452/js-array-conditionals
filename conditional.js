99592c663b84c1daf360a63c9a176a5175e02659
sabbir1452 <sabbirrahman1452@gmail.com>
2023-12-17 13:53:26 +0600

    lskdf

---
+++ conditional.js
@@ -0,0 +1,41 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+// if iphone price is less than my budget. I will buy the iphone
+/**
+ * if iphone price is less than my budget
+ *
+ *      i will buy the iphone
+ *
+ *
+ *
+ * if (conditon) {
+ *     // will execute if the condition is true
+ * }
+ */
+
+if (iphonePrice < myBudget) {
+  console.log("iphone kine futani marbo");
+}
+
+var chickenPrice = 500;
+var myMoney = 5000;
+
+// if (chickenPrice <= myMoney) {
+//   console.log("Murgir raan khamu and haddi chibamu!!!");
+// }
+// if (chickenPrice > myMoney) {
+//   console.log("Smashed potato and lentils soup");
+// }
+
+/**
+ * if chiken er price is less than my budget
+ * i will eat chiken
+ * na hoi: else
+ *
+ */
+
+if (chickenPrice < myMoney) {
+  console.log("I Will eat Chicken");
+} else {
+  console.log("I will eat potato");
+}
